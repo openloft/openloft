7f17507926d29c685908d5fde2343801e71bd4fc
Xiaodong Ye <yeahdongcn@gmail.com>
2023-11-19 13:40:37 +0800

    Update config
    Signed-off-by: Xiaodong Ye <yeahdongcn@gmail.com>

--- controllers/virtualclusterinstance_controller.go
+++ controllers/virtualclusterinstance_controller.go
@@ -42,6 +42,12 @@ type VirtualClusterInstanceReconciler struct {
 //+kubebuilder:rbac:groups=storage.loft.sh,resources=virtualclusterinstances/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=storage.loft.sh,resources=virtualclusterinstances/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=storage.openloft.cn,resources=virtualclusters,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=storage.openloft.cn,resources=virtualclusters/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=storage.openloft.cn,resources=virtualclusters/finalizers,verbs=update
+
+//+kubebuilder:rbac:groups="",resources=namespaces,verbs=create
+
 func (r *VirtualClusterInstanceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx).WithName("VirtualClusterInstanceReconciler")
 
