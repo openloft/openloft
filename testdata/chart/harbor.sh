751e5274a0314fb143284ff76586ea33e53d44ec
Xiaodong Ye <yeahdongcn@gmail.com>
2023-12-18 15:41:01 +0800

    Use HelmChart/HelmChartConfig to install/config harbor chart
    Signed-off-by: Xiaodong Ye <yeahdongcn@gmail.com>

--- testdata/chart/harbor.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-kubectl get secret vc-isolated-sample -n vc-isolated-sample --template={{.data.config}} | base64 -D > kubeconfig.yaml
-export KUBECONFIG=$PWD/kubeconfig.yaml
-helm repo add harbor https://helm.goharbor.io
-helm install apps harbor/harbor
